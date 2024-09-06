c95bd0708952108a1c11c9ba60edb16899f68b68
Kyle Brown <brownk75@winthrop.edu>
2024-09-06 17:17:08 -0400

    completed program

---
+++ helloworld.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello World PHP</title>
+  </head>
+  <body>
+    <?php
+      // PHP echo statement to display "Hello World"
+      echo "Hello World!";
+    ?>
+  </body>
+</html>
